04395c2feede6ee3033b3eb94ac562a8487561a1
Andrey Tsatsyn <tsatsynav@yandex.ru>
2023-11-17 10:27:21 +0300

    add aray task4

--- pages/r_s/module1/array/task4.js
+++ pages/r_s/module1/array/task4.js
@@ -1,13 +1,10 @@
-const coffees = ['Latte', 'Cappuccino', 'Americano'];
+const coffees = ["Latte", "Cappuccino", "Americano"];
 const prices = [1.5, 1, 2];
 
 const updatedPrices = prices.map((item) => {
   return item + 0.5;
 });
 
-coffees.forEach((item) => {
-  for(let i = 0; i < coffees.length && i < updatedPrices.length; i += 1) {
-    console.log(`Кофе ${item} сейчас стоит ${updatedPrices} евро`);
-  }
-}); 
-
+coffees.forEach((item, index) => {
+  alert(`Кофе ${item} сейчас стоит ${updatedPrices[index]} евро`);
+});
